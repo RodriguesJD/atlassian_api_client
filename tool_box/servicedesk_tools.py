afdac88c3804575066acc3531df156cf28fe21d9
Jeremy Rodrigues <jrodrigues@ripple.com>
2020-08-26 12:59:44 -0700

    Added tools for servicedesk.

---
+++ tool_box/servicedesk_tools.py
@@ -0,0 +1,32 @@
+import os
+from pprint import pprint
+
+from atlassian import ServiceDesk
+
+
+base_url = os.environ["JIRA_BASE_URL"]
+username = os.environ["WORK_EMAIL"]
+key = os.environ["JIRA_KEY"]
+
+
+def service_desk_request():
+    sd = ServiceDesk(
+            url=base_url,
+            username=username,
+            password=key)
+
+    return sd
+
+
+def list_all_sd_projects():
+    return service_desk_request().get_service_desks()
+
+
+def list_request_types(service_desk_id):
+    return service_desk_request().get_request_types(service_desk_id)["values"]
+
+
+def create_ticket(service_desk_id, request_type_id, dict_values):
+    return service_desk_request().create_customer_request(service_desk_id=service_desk_id, request_type_id=request_type_id, values_dict=dict_values)
+
+
