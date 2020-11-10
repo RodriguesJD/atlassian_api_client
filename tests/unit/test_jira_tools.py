b56d0b321004020c533b7608fa71da7dd3c15e83
Jeremy Rodrigues <jeremyrodrigues@tank.local>
2020-11-10 12:05:20 -0800

    Create module just for GET requests.

--- tests/unit/test_jira_tools.py
+++ tests/unit/test_jira_tools.py
@@ -3,13 +3,13 @@ import sys
 sys.path.insert(0, os.getcwd())
 
 try:
-    from tool_box import jira_tools
+    from tool_box import jira_get_tools
 except ModuleNotFoundError:
     from atlassian_api_client.tool_box import jira_tools
 
 
 def test_get_all_users():
-    all_users = jira_tools.get_all_users()
+    all_users = jira_get_tools.get_all_users()
     assert isinstance(all_users, list)
 
 
@@ -23,7 +23,7 @@ def test_find_user_by_email():
 
 def test_find_users_groups_by_account_id():
     account_id = os.environ["MY_JIRA_ACCOUNT_ID"]
-    users_groups = jira_tools.find_users_groups_by_account_id(account_id)
+    users_groups = jira_get_tools.find_users_groups_by_account_id(account_id)
     isinstance(users_groups, list)
     for group in users_groups:
         isinstance(group, dict)
@@ -50,7 +50,7 @@ def test_get_user_property():
 
 
 def test_get_all_groups():
-    all_groups = jira_tools.get_all_groups()
+    all_groups = jira_get_tools.get_all_groups()
     groups = all_groups['groups']
     isinstance(all_groups, dict)
     isinstance(groups, list)
@@ -67,7 +67,7 @@ def test_get_all_groups():
 
 def test_users_in_group_by_group_name():
     group_name = 'itsupport'
-    group_members = jira_tools.users_in_group_by_group_name(group_name)
+    group_members = jira_get_tools.users_in_group_by_group_name(group_name)
     isinstance(group_members, list)
     for member in group_members:
         isinstance(member, dict)
